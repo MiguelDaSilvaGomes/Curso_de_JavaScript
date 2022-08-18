0cc4e44e7cbf87cf9af723f9d8b88a7ab7d7f5e3
Migue da Silva Gomes <106116635+MiguelDaSilvaGomes@users.noreply.github.com>
2022-08-18 16:52:58 -0300

    Aulas789

---
+++ Aula7/exercicio.js
@@ -0,0 +1,7 @@
+var vel = 80.1
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel > 80){
+   console.log('Você Ultrapassou a velocidade permitida MULTADO')
+}else{
+   console.log('Tudo em ordem')
+}
